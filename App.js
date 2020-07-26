bf7f250d8f2b29d7a935f298b98a6aad01c876fd
Hüseyin BOZKURT <huseyinbozkuurt@gmail.com>
2020-07-26 18:13:19 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './App/reducers';
+import Router from './App/Router';
+
+export default class App extends React.Component {
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <Router />
+      </Provider>
+    );
+  }
+}
